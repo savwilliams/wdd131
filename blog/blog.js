2e4d4c037456e71a8eeee7717219f7457100d486
savwilliams <wil24021@byui.edu>
2024-10-23 15:26:59 -0600

    added javascript to dynamically generate html. Erased article html in html file. added new article box id on article container (changed from section to div).

--- blog/blog.js
+++ blog/blog.js
@@ -9,7 +9,7 @@ const articles = [
     imgAlt: "Book cover for Septimus Heap 1",
     ages: "10-14",
     genre: "Fantasy",
-    stars: "****",
+    stars: "⭐⭐⭐⭐",
   },
   {
     id: 2,
@@ -24,4 +24,55 @@ const articles = [
     genre: "Fantasy",
     stars: "⭐⭐⭐⭐",
   },
+  {
+    id: 3,
+    title: "Belgariad Book One: Pawn of Prophecy",
+    date: "Feb 12, 2022",
+    description:
+      "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+    imgSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+    imgAlt: "Book cover for Pawn of Prophecy",
+    ages: "12-16",
+    genre: "Fantasy",
+    stars: "⭐⭐⭐⭐⭐"
+    }
 ];
+
+
+const articleBox = document.querySelector('#article-box');
+
+function createArticleHTML(article) {  
+  const articleElement = document.createElement('article');
+  articleElement.classList.add('article-box');
+
+  const articleContent = `
+    <aside class="book-details-aside">
+      <ul class="paragraph-font">
+        <li class="paragraph-font" id="font-size">${article.date}</li>
+        <li class="paragraph-font">${article.ages}</li>
+        <li class="paragraph-font">${article.genre}</li>
+        <li class="paragraph-font">${article.stars}</li>
+      </ul>
+    </aside>
+    <div>
+      <h2 class="book-title">Book Title: ${article.title}</h2>
+      <div class="book-cover-img-box">
+        <img class="book-cover-img" src="${article.imgSrc}" alt="${article.imgAlt}">
+      </div>
+      <h3>Description:</h3>
+      <p class="paragraph-font">${article.description}</p>
+    </div>
+  `;
+  
+  articleElement.innerHTML = articleContent;  
+  articleBox.appendChild(articleElement);
+}
+
+function displayArticles(articles) {
+  articles.forEach(article => {
+    createArticleHTML(article);
+  });
+}
+
+displayArticles(articles);
